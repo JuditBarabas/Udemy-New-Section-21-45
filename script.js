4ecc6c3e7b9ff27f857cfa988afd5613c20c6e13
JuditBarabas <62601033+JuditBarabas@users.noreply.github.com>
2020-10-27 09:03:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const square = {
+    area(side) {
+        return side * side;
+    },
+    perimeter(side) {
+        return side * 4;
+    }
+};
+
+console.log('Test:');
+console.log(square.area(10));
+console.log(square.perimeter(10));
